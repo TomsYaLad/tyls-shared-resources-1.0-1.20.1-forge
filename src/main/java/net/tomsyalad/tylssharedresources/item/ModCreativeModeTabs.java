ff9085f185d58b310b9d5d003d3cd5657b4d4e65
TomsYaLad <tomanr234@gmail.com>
2025-12-31 17:04:10 +1100

    Basic items - Dec2025

--- src/main/java/net/tomsyalad/tylssharedresources/item/ModCreativeModeTabs.java
+++ src/main/java/net/tomsyalad/tylssharedresources/item/ModCreativeModeTabs.java
@@ -17,10 +17,18 @@ public class ModCreativeModeTabs {
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.LUMBER_OAK.get()))
                     .title(Component.translatable("creativetab.tyls_resources_tab"))
                     .displayItems((itemDisplayParameters, output) -> {
+
                         output.accept(ModItems.LUMBER_OAK.get());
-                        output.accept(ModItems.LUMBER_BIRCH.get());
                         output.accept(ModItems.LUMBER_SPRUCE.get());
+                        output.accept(ModItems.LUMBER_BIRCH.get());
+                        output.accept(ModItems.LUMBER_JUNGLE.get());
                         output.accept(ModItems.LUMBER_ACACIA.get());
+                        output.accept(ModItems.LUMBER_DARK_OAK.get());
+                        output.accept(ModItems.LUMBER_MANGROVE.get());
+                        output.accept(ModItems.LUMBER_CHERRY.get());
+                        output.accept(ModItems.LUMBER_CRIMSON.get());
+                        output.accept(ModItems.LUMBER_WARPED.get());
+                        output.accept(ModItems.LUMBER_BAMBOO.get());
 
                         output.accept(ModItems.ROCK.get());
                         output.accept(ModItems.ROCK_DARK.get());
@@ -31,6 +39,12 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.ROCK_TUFF.get());
                         output.accept(ModItems.ROCK_SLATE.get());
 
+                        output.accept(ModItems.SHARD_FLINT.get());
+                        output.accept(ModItems.SHARD_GLASS.get());
+                        output.accept(ModItems.SHARD_ICE.get());
+
+                        output.accept(ModItems.PILE_DIRT.get());
+                        output.accept(ModItems.PILE_MUD.get());
                         output.accept(ModItems.PILE_SAND.get());
                         output.accept(ModItems.PILE_SAND_RED.get());
                         output.accept(ModItems.PILE_SAND_SOUL.get());
@@ -39,6 +53,13 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.PILE_GRAVEL_RUSTIC.get());
 
                         output.accept(ModItems.BARK_PIECE.get());
+                        output.accept(ModItems.LEAVES.get());
+                        output.accept(ModItems.BAMBOO_STRIPPED.get());
+                        output.accept(ModItems.STRAW.get());
+                        output.accept(ModItems.CUT_REEDS.get());
+                        output.accept(ModItems.BAGASSE.get());
+                        output.accept(ModItems.ROTTEN_PLANT.get());
+                        output.accept(ModItems.MANURE.get());
 
                         output.accept(ModItems.HIDE_COARSE.get());
                         output.accept(ModItems.HIDE_SCRAPED.get());
@@ -46,25 +67,71 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.LEATHER_TANNED.get());
                         output.accept(ModItems.LEATHER_FANCY.get());
                         output.accept(ModItems.FUR.get());
+                        output.accept(ModItems.FUR_PALE.get());
+                        output.accept(ModItems.FEATHER_TROPICAL.get());
+                        output.accept(ModItems.BARB.get());
+                        output.accept(ModItems.TOXINS.get());
+                        output.accept(ModItems.SHARD_BONE.get());
+                        output.accept(ModItems.PEARL.get());
 
                         output.accept(ModItems.WITHER_BONE.get());
                         output.accept(ModItems.NETHER_ASH.get());
 
                         output.accept(ModItems.ORE_TAILINGS.get());
-                        output.accept(ModItems.CEMENT.get());
-                        output.accept(ModItems.RAW_GLASS.get());
+                        output.accept(ModItems.SULFUR.get());
+                        output.accept(ModItems.NITRE.get());
                         output.accept(ModItems.RAW_COPPER_NUGGET.get());
                         output.accept(ModItems.RAW_IRON_NUGGET.get());
                         output.accept(ModItems.RAW_GOLD_NUGGET.get());
-                        output.accept(ModItems.COPPER_NUGGET.get());
+                        output.accept(ModItems.SHARD_DIAMOND.get());
+                        output.accept(ModItems.RAW_TIN_NUGGET.get());
+                        output.accept(ModItems.RAW_LEAD_NUGGET.get());
+                        output.accept(ModItems.RAW_SILVER_NUGGET.get());
+
                         output.accept(ModItems.CLAY_BOWL.get());
+                        output.accept(ModItems.CLAY_BRICK.get());
+                        output.accept(ModItems.CEMENT.get());
+                        output.accept(ModItems.RAW_GLASS.get());
+
+                        output.accept(ModItems.PILE_FLOUR_WHEAT.get());
+                        output.accept(ModItems.SUGAR_RAW.get());
+                        output.accept(ModItems.DOUGH.get());
+                        output.accept(ModItems.CAKE_BATTER.get());
+
+                        output.accept(ModItems.BRICK_STONE.get());
+                        output.accept(ModItems.BRICK_SNOW.get());
+                        output.accept(ModItems.BRICK_ICE.get());
+                        output.accept(ModItems.FRAME_WOOD.get());
+                        output.accept(ModItems.FRAME_METAL.get());
 
                         output.accept(ModItems.THREAD.get());
-                        output.accept(ModItems.WOOL_CLOTH.get());
+                        output.accept(ModItems.CLOTH_WOOL.get());
+                        output.accept(ModItems.CLOTH_PLANT.get());
                         output.accept(ModItems.QUILL.get());
-                        output.accept(ModItems.WOODEN_FRAMING.get());
+                        output.accept(ModItems.ARROWHEAD.get());
+                        output.accept(ModItems.COPPER_NUGGET.get());
+                        output.accept(ModItems.STEEL_INGOT.get());
+                        output.accept(ModItems.STEEL_NUGGET.get());
+                        output.accept(ModItems.STEEL_SHEET.get());
+                        output.accept(ModItems.NAIL.get());
+                        output.accept(ModItems.NETHERITE_NUGGET.get());
                         output.accept(ModItems.HINGE.get());
+                        output.accept(ModItems.SPRING.get());
+                        output.accept(ModItems.SPRING_LOADED.get());
+                        output.accept(ModItems.WIRE_COPPER.get());
+                        output.accept(ModItems.WIRE_IRON.get());
+                        output.accept(ModItems.WIRE_STEEL.get());
+                        output.accept(ModItems.WIRE_GOLD.get());
+                        output.accept(ModItems.TRANSISTOR.get());
+                        output.accept(ModItems.CIRCUIT_BOARD.get());
+                        output.accept(ModItems.RECIEVER.get());
 
+                        output.accept(ModItems.SCRAP_WOOD.get());
+                        output.accept(ModItems.SCRAP_METAL.get());
+                        output.accept(ModItems.SCRAP_ELECTRICAL.get());
+                        output.accept(ModItems.RUBBLE.get());
+                        output.accept(ModItems.CLOTH_OLD.get());
+                        output.accept(ModItems.JUNK.get());
 
                     })
                     .build());
--- src/main/java/net/tomsyalad/tylssharedresources/item/ModItems.java
+++ src/main/java/net/tomsyalad/tylssharedresources/item/ModItems.java
@@ -13,11 +13,25 @@ public class ModItems {
 
     public static final RegistryObject<Item> LUMBER_OAK = ITEMS.register("lumber_oak",
             () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> LUMBER_BIRCH = ITEMS.register("lumber_birch",
-            () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> LUMBER_SPRUCE = ITEMS.register("lumber_spruce",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LUMBER_BIRCH = ITEMS.register("lumber_birch",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LUMBER_JUNGLE = ITEMS.register("lumber_jungle",
+            () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> LUMBER_ACACIA = ITEMS.register("lumber_acacia",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LUMBER_DARK_OAK = ITEMS.register("lumber_dark_oak",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LUMBER_MANGROVE = ITEMS.register("lumber_mangrove",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LUMBER_CHERRY = ITEMS.register("lumber_cherry",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LUMBER_CRIMSON = ITEMS.register("lumber_crimson",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LUMBER_WARPED = ITEMS.register("lumber_warped",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LUMBER_BAMBOO = ITEMS.register("lumber_bamboo",
             () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> ROCK = ITEMS.register("rock",
@@ -37,6 +51,17 @@ public class ModItems {
     public static final RegistryObject<Item> ROCK_SLATE = ITEMS.register("rock_slate",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> SHARD_FLINT = ITEMS.register("shard_flint",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SHARD_GLASS = ITEMS.register("shard_glass",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SHARD_ICE = ITEMS.register("shard_ice",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PILE_DIRT = ITEMS.register("pile_dirt",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PILE_MUD = ITEMS.register("pile_mud",
+            () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> PILE_SAND = ITEMS.register("pile_sand",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> PILE_SAND_RED = ITEMS.register("pile_sand_red",
@@ -52,6 +77,20 @@ public class ModItems {
 
     public static final RegistryObject<Item> BARK_PIECE = ITEMS.register("bark_piece",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LEAVES = ITEMS.register("leaves",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BAMBOO_STRIPPED = ITEMS.register("bamboo_stripped",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STRAW = ITEMS.register("straw",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CUT_REEDS = ITEMS.register("cut_reeds",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BAGASSE = ITEMS.register("bagasse",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ROTTEN_PLANT = ITEMS.register("rotten_plant",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MANURE = ITEMS.register("manure",
+            () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> HIDE_COARSE = ITEMS.register("hide_coarse",
             () -> new Item(new Item.Properties()));
@@ -65,6 +104,18 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> FUR = ITEMS.register("fur",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FUR_PALE = ITEMS.register("fur_pale",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FEATHER_TROPICAL = ITEMS.register("feather_tropical",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BARB = ITEMS.register("barb",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TOXINS = ITEMS.register("toxins",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SHARD_BONE = ITEMS.register("shard_bone",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PEARL = ITEMS.register("pearl",
+            () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> WITHER_BONE = ITEMS.register("wither_bone",
             () -> new Item(new Item.Properties()));
@@ -73,9 +124,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> ORE_TAILINGS = ITEMS.register("ore_tailings",
             () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> CEMENT = ITEMS.register("cement",
+    public static final RegistryObject<Item> SULFUR = ITEMS.register("sulfur",
             () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> RAW_GLASS = ITEMS.register("raw_glass",
+    public static final RegistryObject<Item> NITRE = ITEMS.register("nitre",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_COPPER_NUGGET = ITEMS.register("raw_copper_nugget",
             () -> new Item(new Item.Properties()));
@@ -83,20 +134,119 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_GOLD_NUGGET = ITEMS.register("raw_gold_nugget",
             () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> COPPER_NUGGET = ITEMS.register("copper_nugget",
+    public static final RegistryObject<Item> SHARD_DIAMOND = ITEMS.register("shard_diamond",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_TIN_NUGGET = ITEMS.register("raw_tin_nugget",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_LEAD_NUGGET = ITEMS.register("raw_lead_nugget",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SILVER_NUGGET = ITEMS.register("raw_silver_nugget",
+            () -> new Item(new Item.Properties()));
+
     public static final RegistryObject<Item> CLAY_BOWL = ITEMS.register("clay_bowl",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CLAY_BRICK = ITEMS.register("clay_brick",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CEMENT = ITEMS.register("cement",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_GLASS = ITEMS.register("raw_glass",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PILE_FLOUR_WHEAT = ITEMS.register("pile_flour_wheat",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SUGAR_RAW = ITEMS.register("sugar_raw",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DOUGH = ITEMS.register("dough",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CAKE_BATTER = ITEMS.register("cake_batter",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRICK_STONE = ITEMS.register("brick_stone",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BRICK_SNOW = ITEMS.register("brick_snow",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BRICK_ICE = ITEMS.register("brick_ice",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FRAME_WOOD = ITEMS.register("frame_wood",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FRAME_METAL = ITEMS.register("frame_metal",
             () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> THREAD = ITEMS.register("thread",
             () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> WOOL_CLOTH = ITEMS.register("wool_cloth",
+    public static final RegistryObject<Item> CLOTH_WOOL = ITEMS.register("cloth_wool",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CLOTH_PLANT = ITEMS.register("cloth_plant",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> QUILL = ITEMS.register("quill",
             () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> WOODEN_FRAMING = ITEMS.register("wooden_framing",
+    public static final RegistryObject<Item> ARROWHEAD = ITEMS.register("arrowhead",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> COPPER_NUGGET = ITEMS.register("copper_nugget",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STEEL_INGOT = ITEMS.register("steel_ingot",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STEEL_NUGGET = ITEMS.register("steel_nugget",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STEEL_SHEET = ITEMS.register("steel_sheet",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> NETHERITE_NUGGET = ITEMS.register("netherite_nugget",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> NAIL = ITEMS.register("nail",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> HINGE = ITEMS.register("hinge",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SPRING = ITEMS.register("spring",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SPRING_LOADED = ITEMS.register("spring_loaded",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> WIRE_COPPER = ITEMS.register("wire_copper",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> WIRE_IRON = ITEMS.register("wire_iron",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> WIRE_STEEL = ITEMS.register("wire_steel",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> WIRE_GOLD = ITEMS.register("wire_gold",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TRANSISTOR = ITEMS.register("transistor",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CIRCUIT_BOARD = ITEMS.register("circuit_board",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RECIEVER = ITEMS.register("reciever",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SCRAP_WOOD = ITEMS.register("scrap_wood",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SCRAP_METAL = ITEMS.register("scrap_metal",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SCRAP_ELECTRICAL = ITEMS.register("scrap_electrical",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RUBBLE = ITEMS.register("rubble",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CLOTH_OLD = ITEMS.register("cloth_old",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> JUNK = ITEMS.register("junk",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_ONE = ITEMS.register("placeholder_item_one",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_TWO = ITEMS.register("placeholder_item_two",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_THREE = ITEMS.register("placeholder_item_three",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_FOUR = ITEMS.register("placeholder_item_four",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_FIVE = ITEMS.register("placeholder_item_five",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_SIX = ITEMS.register("placeholder_item_six",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_SEVEN = ITEMS.register("placeholder_item_seven",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_EIGHT = ITEMS.register("placeholder_item_eight",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_NINE = ITEMS.register("placeholder_item_nine",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> PLACEHOLDER_ITEM_TEN = ITEMS.register("placeholder_item_ten",
             () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
